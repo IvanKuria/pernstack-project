0d8489faaa66fa136732e51cb5f11dde02a48804
Ivan Kuria <ivankuria7@gmail.com>
2025-05-08 13:06:59 -0700

    finished configuring the routes. Tested using Postman. Postgresql is so much more intuitive than mongodb

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -9,8 +9,8 @@ export const getProducts = async (req, res) => {
         console.log("fetched products", products);
         res.status(200).json({success: true, data: products})  ;
     } catch (error) {
-        console.log("Error fetching all products", error);
-        res.send(500).json({success: false, message: "Internal Server Error"})
+        console.log("Error in getProducts function", error);
+        res.status(500).json({success: false, message: "Internal Server Error"})
     }
 };
 
@@ -18,17 +18,73 @@ export const createProduct = async (req, res) => {
     const { name, price, image } = req.body;
 
     if (!name || !price || !image) {
-        res.send(400).json({success: false, message: "All fields are required"})
+        res.status(400).json({success: false, message: "All fields are required"})
     }
 
     try {
-         
+        const newProduct = await sql`
+            INSERT INTO products (name, price, image)
+            VALUES (${name},${price},${image})  
+            RETURNING * 
+            `
+        console.log("new product added: ", newProduct);
+        res.status(201).json({success: true, data: newProduct[0]});
     } catch (error) {
-        
+        console.log("Error in createProduct function", error);
+        res.status(500).json({success: false, message: "Internal Server Error"})
     }
 };
 
+export const getProduct = async (req, res) => {
+    const { id } = req.params;
 
-export const getProduct = async (req, res) => {};
-export const updateProduct = async (req, res) => {};
-export const deleteProduct = async (req, res) => {};
+    try {
+         const product = await sql`
+            SELECT * FROM products WHERE id=${id}
+         `;
+         res.status(200).json({success: true, data: product[0]})
+    } catch (error) {
+        console.log("Error in getProduct function", error);
+        res.status(500).json({success: false, message: "Internal Server Error"})
+    }
+};
+ 
+export const updateProduct = async (req, res) => {
+    const { name, price, image } = req.body;
+    const { id } = req.params;
+    
+    try {
+        const updateProduct = await sql`
+            UPDATE products
+            SET name=${name}, price=${price}, image=${image}
+            WHERE id=${id}
+            RETURNING *
+        `
+        if (updateProduct.length === 0) {
+            return res.status(404).json({success: false, message: "Product not found"});
+        }
+        res.status(200).json({success: true, data: updateProduct[0]});
+    } catch (error) {
+        console.log("Error in updateProduct function", error);
+        res.status(500).json({success: false, message: "Internal Server Error"})
+    }
+};
+
+export const deleteProduct = async (req, res) => {
+    const { id } = req.params;
+    
+    try {
+        const deleteProduct = await sql`
+            DELETE FROM products
+            WHERE id=${id}
+            RETURNING * 
+        `
+        if (deleteProduct.length === 0) {
+            return res.status(404).json({success: false, message: "Product not found"});
+        }
+        res.status(200).json({success: true, data: deleteProduct[0]});
+    } catch (error) {
+        console.log("Error in deleteProduct function", error);
+        res.status(500).json({success: false, message: "Internal Server Error"})
+    }
+};
